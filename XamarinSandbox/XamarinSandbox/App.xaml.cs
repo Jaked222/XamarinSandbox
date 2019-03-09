88d08c64a94e27045fd626a777c8bb5ddc02f3e5
Jake Duncan <jakecduncan@gmail.com>
2019-03-09 16:58:37 -0500

    Simple stack layout

--- XamarinSandbox/XamarinSandbox/App.xaml.cs
+++ XamarinSandbox/XamarinSandbox/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XamarinSandbox
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new StackPage();
         }
 
         protected override void OnStart()
